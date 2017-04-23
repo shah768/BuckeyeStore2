42f332b24d12453856c2136ce9e48afef2d49c5d
shah768 <shah.768@osu.edu>
2017-04-23 14:34:24 -0400

    lab1

---
+++ Source/BuckeyeStore.Core/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuckeyeStore.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public Review Review { get; set; }
+        public AggregateRating AggregateRating { get; set; }
+        public List<Offer> Offers { get; set; }
+        public string Sku { get; set; }
+
+        public void AddReview(Review review)
+        {
+            //throw new NotImplementedException();
+            this.Review = review;
+        }
+    }
+}
